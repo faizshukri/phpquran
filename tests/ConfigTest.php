07d8a33e20e266ba993955cec8c67708322dbb2f
Faiz Shukri <faizshukri90@gmail.com>
2016-02-07 16:55:09 +0800

    Fix config replace deep

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,7 +8,17 @@ class ConfigTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->config = new Config(['level1' => 'yes', 'level2' => ['cool' => 'nice', 'great' => ['fantastic' => 'deep']]]);
+        $this->config = new Config([
+            'level1' => 'yes',
+            'level2' => [
+                'cool' => 'nice', 'great' => [
+                    'fantastic' => 'deep'
+                ]
+            ],
+            'limit' => [
+                'ayah' => '5'
+            ]
+        ]);
     }
 
     public function test_get()
@@ -25,5 +35,11 @@ class ConfigTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('nice', $this->config->get('level2.cool'));
         $this->assertEquals('deep', $this->config->get('level2.great.fantastic'));
+    }
+
+    public function test_build_merge_deep()
+    {
+        $this->assertEquals( 5, $this->config->get('limit.ayah') );
+        $this->assertEquals( 3, $this->config->get('limit.translation') );
     }
 }
